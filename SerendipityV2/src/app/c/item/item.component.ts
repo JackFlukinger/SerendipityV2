2cb8f417b27c3f3f19660409c02ce222cf2dc08a
Rexford Essilfie <rexfordessilfie@Rexfords-MacBook-Pro.local>
2019-07-11 11:03:20 -0500

    Attempts to make front-end for item.

--- SerendipityV2/src/app/c/item/item.component.ts
+++ SerendipityV2/src/app/c/item/item.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-random',
+  selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.scss']
 })
