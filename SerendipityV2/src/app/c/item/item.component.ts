b64890abc3d9484539dc5b334af7c83733926969
Jack Flukinger <jackflukinger@gmail.com>
2019-07-11 11:04:01 -0500

    Minor item service addition

--- SerendipityV2/src/app/c/item/item.component.ts
+++ SerendipityV2/src/app/c/item/item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemService, Item } from '../../s/item.service';
 
 @Component({
   selector: 'app-random',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private ItemService:ItemService
+  ) { }
 
   ngOnInit() {
   }
